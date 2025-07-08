b90bcd08ce72dc5f66141da96531166a5082dd59
Stefano <s.olivero.3215@vallauri.edu>
2025-07-08 14:23:30 +0200

    Add menuToggle for programming languages

--- Web/index.js
+++ Web/index.js
@@ -1,5 +1,19 @@
 "use strict"
 
 window.onload = function() {
+    const arrowDownIcon = document.querySelector("#itDetails i");
     // window.scrollTo({"top": 0, "behavior": "smooth"});
+    let isOpened = false;
+
+    itDetails.addEventListener("click", function() {
+        isOpened = !isOpened;
+        if (isOpened) {
+            programmingLanguages.classList.remove("d-none");
+            arrowDownIcon.classList.remove("d-none");
+            return;
+        }
+
+        programmingLanguages.classList.add("d-none");
+        arrowDownIcon.classList.add("d-none");
+    });
 }

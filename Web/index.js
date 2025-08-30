eb996443504d2d2c2de3c0dfc2b8e758ddfcc524
Stefano Olivero <s.olivero.3215@vallauri.edu>
2025-08-30 12:59:23 +0200

    Added project videos and minor changes
    Warning: videos are not commited. Consider upload them online

--- Web/index.js
+++ Web/index.js
@@ -2,18 +2,26 @@
 
 window.onload = function() {
     const arrowDownIcon = document.querySelector("#itDetails i");
+    const videos = document.getElementsByTagName("video");
     // window.scrollTo({"top": 0, "behavior": "smooth"});
+
+    for (const video of videos) {
+        video.addEventListener("click", function() { this.requestFullscreen(); });
+    }
+
     let isOpened = false;
+    $(programmingLanguages).hide();
 
     itDetails.addEventListener("click", function() {
         isOpened = !isOpened;
         if (isOpened) {
-            programmingLanguages.classList.remove("d-none");
+            // programmingLanguages.classList.remove("d-none");
+            $(programmingLanguages).slideDown(1500);
             arrowDownIcon.classList.remove("d-none");
             return;
         }
 
-        programmingLanguages.classList.add("d-none");
+        $(programmingLanguages).slideUp(1500);
         arrowDownIcon.classList.add("d-none");
     });
 }
